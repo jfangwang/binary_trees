14efeae4966a613cf7f44cd858ad81089b9e9864
Mfeliciano778 <1652@holbertonschool.com>
2021-03-08 21:33:22 +0000

    Task 13

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - goes through tree 
+ * @node: a node
+ * Return: 1 if a node is a leaf, 0 if not
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree == NULL || (!(tree->left) && !(tree->right)))
+		return (0);
+	return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+}
