d67693928cac59a341bd4aa8ef5f55cd36db1dbf
jfangwang <qbs1864@gmail.com>
2021-03-08 19:42:45 +0000

    task 5 done

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - a
+ * @node: Value
+ * Return: A pointer to the new node or NULL
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->parent == NULL)
+		return (1);
+	return (0);
+}
