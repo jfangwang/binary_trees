74ee2d1c98e49c1c70cd2ad216eb196c20799d05
Mfeliciano778 <1652@holbertonschool.com>
2021-03-08 19:14:55 +0000

    Added task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,15 +12,18 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	if (parent == NULL)
 		return NULL;
-	new_node = malloc(sizeof(binary_tree_t));
+	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
 		return NULL;
 
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
+		new_node->right->n = parent->right->n;
+		new_node->right->parent = new_node;
+	}
 	parent->right = new_node;
 	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
-	new_node->n = value;
 
 	return (new_node);
 }
