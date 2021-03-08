f9564f54b45586da33e981f132237dbaeb2b51dc
jfangwang <qbs1864@gmail.com>
2021-03-08 18:17:05 +0000

    setup header file and started 0-binary

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - a
+ * @parent: parent
+ * @value: Value
+ * Return: A pointer to the new node or NULL
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	return (NULL);
+}
