c386bbb169be0ff977fe736a352defabc5d77785
jfangwang <qbs1864@gmail.com>
2021-03-08 21:05:18 +0000

    working on task 13

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - a
+ * @tree: parent
+ * Return: A pointer to the new node or NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	else
+	{
+		if (tree->left != NULL)
+			return (1 + binary_tree_height(tree->left));
+		if (tree->right != NULL)
+			return (1 + binary_tree_height(tree->right));
+	}
+	return (0);
+}
