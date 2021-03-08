eff7933a6a3c4c39962d36a60f56233d4d9b5495
jfangwang <qbs1864@gmail.com>
2021-03-08 19:23:08 +0000

    task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - a
+ * @tree: parent
+ * Return: A pointer to the new node or NULL
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree != NULL)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
+	}
+}
