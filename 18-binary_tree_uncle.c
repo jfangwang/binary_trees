89ed57e2875ef5246e43ebce7b67e24e8b0849b8
Mfeliciano778 <1652@holbertonschool.com>
2021-03-09 19:28:55 +0000

    Task 18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - goes through tree 
+ * @node: a node
+ * Return: 1 if a node is a leaf, 0 if not
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *parent, *gparent;
+
+	if (node == NULL)
+		return (NULL);
+	if (node->parent == NULL)
+		return (NULL);
+	if (node->parent->parent == NULL)
+		return (NULL);
+	parent = node->parent;
+	gparent = node->parent->parent;
+	if (parent == gparent->left)
+		return (gparent->right);
+	return (gparent->left);
+}
