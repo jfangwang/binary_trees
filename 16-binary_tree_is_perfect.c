3a60a5c47a717b682eda53a280cfcbdd5a8bb64f
Mfeliciano778 <1652@holbertonschool.com>
2021-03-09 19:14:05 +0000

    Task 16

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - goes through tree 
+ * @node: a node
+ * Return: 1 if a node is a leaf, 0 if not
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (!(tree->left) && !(tree->right))
+		return (1);
+	if (binary_tree_height(tree->left) != binary_tree_height(tree->left))
+		return (0);
+
+	return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+}
+
+
+/**
+ * binary_tree_node - a
+ * @tree: parent
+ * Return: A pointer to the new node or NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL || tree->right == NULL)
+		return (0);
+
+	if (binary_tree_height(tree->left) > binary_tree_height(tree->right))
+		return (1 + binary_tree_height(tree->left));
+	return (1 + binary_tree_height(tree->right));
+}
