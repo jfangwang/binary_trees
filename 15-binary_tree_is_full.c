d16e1c0b9a6fc38c7ae76ca4d1e26ae15194b750
Mfeliciano778 <1652@holbertonschool.com>
2021-03-10 18:50:06 +0000

    Added a check for tree being null

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -6,6 +6,8 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (1);
 	/* Base case for leaf node */
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
