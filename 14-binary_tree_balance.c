63adba62bc906615ae04857e463588e0e15206d7
Mfeliciano778 <1652@holbertonschool.com>
2021-03-10 18:35:56 +0000

    Added new height function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,13 +6,18 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	int right_sum = 0;
+	int left_sum = 0;
+
 	if (tree == NULL)
 		return (0);
-	if (tree->left != NULL)
-		return (1 + binary_tree_height(tree->left));
-	if (tree->right != NULL)
-		return (1 + binary_tree_height(tree->right));
-	return (1);
+	if (tree->right)
+		right_sum = 1 + binary_tree_height(tree->right);
+	if (tree->left)
+		left_sum = 1 + binary_tree_height(tree->left);
+	if (left_sum > right_sum)
+		return (left_sum);
+	return (right_sum);
 }
 /**
  * binary_tree_balance - a
@@ -23,6 +28,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
+	if (tree->left == NULL)
 	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 	/*return (binary_tree_height(tree->right));*/
 }
