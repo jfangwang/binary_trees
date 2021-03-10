77807dc7507744ee29b166df1a2266c0f45bf0df
Mfeliciano778 <1652@holbertonschool.com>
2021-03-10 19:25:04 +0000

    Advanced task 1

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -8,23 +8,28 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 				     const binary_tree_t *second)
 {
-	binary_tree_t *left;
-	binary_tree_t *right;
-	binary_tree_t *temp;
+	size_t depth_1, depth_2;
 
-	left = first;
-	right = second;
-	temp = right;
-
-	while (left->parent != NULL)
-	{
-		right = temp;
-		while (right->parent != NULL)
-		{
-			if (right->n == left->n)
-				return (right);
-			right = right->parent;
-		}
-	}
-	return (left);
+	if (!(first) || !(second))
+		return (NULL);
+	if (first == second)
+		return ((binary_tree_t *)first);
+	depth_1 = binary_tree_depth(first);
+	depth_2 = binary_tree_depth(second);
+	if (depth_1 > depth_2)
+		return (binary_trees_ancestor(first->parent, second));
+	if (depth_2 > depth_1)
+		return (binary_trees_ancestor(first, second->parent));
+	return (binary_trees_ancestor(first->parent, second->parent));
+}
+/**
+ * binary_tree_depth - goes through tree
+ * @tree: tree node
+ * Return: 1 if a node is a leaf, 0 if not
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL || tree->parent == NULL)
+		return (0);
+	return (1 + binary_tree_depth(tree->parent));
 }
