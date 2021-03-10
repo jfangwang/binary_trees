e2605df8e1b9007b95262d4534f3825b0db5afde
Mfeliciano778 <1652@holbertonschool.com>
2021-03-10 18:54:59 +0000

    Fixed dumb change in 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -11,7 +11,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	if (!(tree->left) && !(tree->right))
 		return (1);
-	if (binary_tree_height(tree->left) != binary_tree_height(tree->left))
+	if (binary_tree_height(tree->left) != binary_tree_height(tree->right))
 		return (0);
 
 	return (binary_tree_is_perfect(tree->left) &&
